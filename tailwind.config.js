e1a4b21840a0ae73c04e2a585d86d30b00117e6d
Ivo Angelov <jagdcake@tuta.io>
2019-08-30 14:42:47 +0300

    Adjust page layout on smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
     theme: {
-        extend: {
+      extend: {
             colors: {
                 'secondary-brown': '#c28236',
                 'eerie': '#22181c',
                 'primary-orange': '#ca4913',
-            }
+            },
+            screens: {
+                  'sm': '360px',
+            },
         }
     },
     variants: {},
