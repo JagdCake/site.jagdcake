98ae08bb788f8ac242bc44a7b64ad765980c443f
Ivo Angelov <jagdcake@tuta.io>
2019-08-31 10:03:00 +0300

    Format configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,16 @@ module.exports = {
         boxShadow: {
             outline: '0 0 0 3px rgba(202,73,19,0.7)',
         },
-      extend: {
+        extend: {
             colors: {
                 'secondary-brown': '#ffc681',
                 'eerie': '#22181c',
                 'primary-orange': '#ca4913',
             },
-          spacing: {
-            '72': '18rem',
-            '80': '20rem',
-          },
+            spacing: {
+                '72': '18rem',
+                '80': '20rem',
+            },
         }
     },
     variants: {},
